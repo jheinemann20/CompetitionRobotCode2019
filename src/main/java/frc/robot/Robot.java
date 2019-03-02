93c99077be74fed1fef65087d9af0d0193edf004
James Heinemann <jheinemann20@setoncatholic.org>
2019-03-02 11:39:15 -0700

    more descriptive comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,7 +36,7 @@ import edu.wpi.first.wpilibj.drive.MecanumDrive;
 public class Robot extends TimedRobot {
   public CANSparkMax fL, fR, rL, rR;
   public VictorSPX l1, l2, e, h1, h2, b, lD;
-  public Joystick myJoy;
+  public Joystick myJoy, myJoy2;
   public MecanumDrive myDrive;
   public DoubleSolenoid shifter;
   public boolean shiftToggle, debounce, debounce_two;
@@ -87,8 +87,9 @@ public class Robot extends TimedRobot {
     rL.setInverted(false);
     rR.setInverted(false);
 
-    // initialize joystick
+    // initialize joysticks
     myJoy = new Joystick(0);
+    myJoy2 = new Joystick(1);
 
     // initialize drivetrain
     myDrive = new MecanumDrive(fL, fR, rL, rR);
@@ -131,7 +132,7 @@ public class Robot extends TimedRobot {
      * 5 - Right stick Y
      */
 
-    // set up joystic axis values
+    // set up joystic axis values and adds deadband
     joyX = addDeadband(myJoy.getRawAxis(1));
     joyY = addDeadband(myJoy.getRawAxis(0));
     joyZ = addDeadband(myJoy.getRawAxis(4));
@@ -140,24 +141,24 @@ public class Robot extends TimedRobot {
     double l1Speed = 0;
     double l2Speed = 0;
 
-    if (myJoy.getRawButton(10)) 
+    if (myJoy.getRawButton(10)) // front lifter up
       l1Speed += 0.25;
 
-    if (myJoy.getRawButton(10))
+    if (myJoy.getRawButton(10)) // rear lifter up
       l2Speed += 0.25;
 
-    if (myJoy.getRawButton(10)) {
+    if (myJoy.getRawButton(10)) { // both lifters up
       l1Speed += 0.25;
       l2Speed += 0.25;
     }
 
-    if (myJoy.getRawButton(10)) 
+    if (myJoy.getRawButton(10)) // front lifter down
       l1Speed -= 0.25;
 
-    if (myJoy.getRawButton(10))
+    if (myJoy.getRawButton(10)) // rear lifter down
       l2Speed -= 0.25;
 
-    if (myJoy.getRawButton(10)) {
+    if (myJoy.getRawButton(10)) { // both lifters down
       l1Speed -= 0.25;
       l2Speed -= 0.25;
     }
@@ -166,10 +167,10 @@ public class Robot extends TimedRobot {
     l2.set(ControlMode.PercentOutput, l2Speed);
 
     // control herders
-    if (myJoy.getRawButton(10)) {
+    if (myJoy.getRawButton(10)) { // herders out
       h1.set(ControlMode.PercentOutput, 0.5);
       h2.set(ControlMode.PercentOutput, 0.5);
-    } else if (myJoy.getRawButton(10)) {
+    } else if (myJoy.getRawButton(10)) { // herders in
       h1.set(ControlMode.PercentOutput, -0.5);
       h2.set(ControlMode.PercentOutput, -0.5);
     } else {
@@ -178,34 +179,36 @@ public class Robot extends TimedRobot {
     }
 
     // control elevator
-    if (myJoy.getRawButton(2))
+    if (myJoy.getRawButton(2)) // elevator up
       e.set(ControlMode.PercentOutput, 0.25);
-    else if (myJoy.getRawButton(3))
+    else if (myJoy.getRawButton(3)) // elevator down
       e.set(ControlMode.PercentOutput, -0.25);
     else
       e.set(ControlMode.PercentOutput, 0);
 
     // control ball holder
-    if (myJoy.getRawButton(10))
+    if (myJoy.getRawButton(10)) // holder out
       b.set(ControlMode.PercentOutput, 0.25);
-    else if (myJoy.getRawButton(10))
+    else if (myJoy.getRawButton(10)) // holder in
       b.set(ControlMode.PercentOutput, -0.25);
     else
       b.set(ControlMode.PercentOutput, 0);
 
-    if (myJoy.getRawButton(10) && !debounce_two) {
+    // control drive toggle
+    if (myJoy.getRawButton(10) && !debounce_two) { // drive toggle
       driveToggle = !driveToggle;
       debounce_two = true;
     } else
       debounce_two = false;
 
+    // control robotDrive
     if (driveToggle) {
       lD.set(ControlMode.PercentOutput, joyY);
-      if (myJoy.getRawButton(1))
+      if (myJoy.getRawButton(1)) // disable driveToggle on press (and shifts)
         driveToggle = false;
     } else {
       // toggles shifter
-      if (myJoy.getRawButton(1)) {
+      if (myJoy.getRawButton(1)) {  // shifter (toggle)
         if (!debounce)
           shiftToggle = !shiftToggle;
         debounce = true;
@@ -214,10 +217,10 @@ public class Robot extends TimedRobot {
       
       if (shiftToggle) {
         shifter.set(Value.kForward);
-        myDrive.driveCartesian(-joyX, 0, joyZ);
+        myDrive.driveCartesian(-joyX, 0, joyZ); // arcade (mecanum without the Y)
       } else {
         shifter.set(Value.kReverse);
-        myDrive.driveCartesian(-joyX, joyY, joyZ);
+        myDrive.driveCartesian(-joyX, joyY, joyZ); // mecanum (with the Y)
       }
     }
   }
